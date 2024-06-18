b8c8bf75a38bbcf90a4c73178ee77ae9ed04d41a
basiaz79 <bzasowski79@gmail.com>
2024-06-18 22:58:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+//generuje losowy kolor w formacie szesnastkowym (hex).
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
   
+// pobiera referencje do przycisków "Start" i "Stop" z dokumentu HTML
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
+
+//jest flagą, która wskazuje, czy zmiana koloru jest aktualnie w toku.
 let isChangingColor = false;
+//przechowuje identyfikator timeoutu zwrócony przez setTimeout
 let timeoutId;
   
+//zmienia kolor tła strony na losowy kolor co sekundę
 function changeBackgroundColor() {
   if (isChangingColor) {
     document.body.style.backgroundColor = getRandomHexColor();
@@ -14,6 +20,7 @@ function changeBackgroundColor() {
   }
 }
   
+//zmiana koloru jest w toku i dezaktywuje przycisk "Start", a aktywuje przycisk "Stop".
 startButton.addEventListener('click', () => {
   if (!isChangingColor) {
     isChangingColor = true;
