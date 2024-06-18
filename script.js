91f7ab691963dbbabc5ae1aca9d227d30370f3d8
basiaz79 <bzasowski79@gmail.com>
2024-06-18 22:04:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
-function getRandomHexColor() {  
+function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    }
-
+}
+  
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
 let isChangingColor = false;
-
+let timeoutId;
+  
 function changeBackgroundColor() {
-    // 
+  if (isChangingColor) {
     document.body.style.backgroundColor = getRandomHexColor();
-    setTimeout(changeBackgroundColor, 1000);
+    timeoutId = setTimeout(changeBackgroundColor, 1000);
+  }
 }
-
+  
 startButton.addEventListener('click', () => {
-    // 
-       isChangingColor = true;
-       // 
-       changeBackgroundColor();
-       
-    //
-});
-
-stopButton.addEventListener('click', () => {
+  if (!isChangingColor) {
+    isChangingColor = true;
+    changeBackgroundColor();
+    startButton.disabled = true;
     stopButton.disabled = false;
-    // 
+  }
+});
+  
+stopButton.addEventListener('click', () => {
+  if (isChangingColor) {
+    isChangingColor = false;
+    clearTimeout(timeoutId);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+  }
 });
